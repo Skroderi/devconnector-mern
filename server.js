e6a9eba650ba14c522fab5556f98e804b47cd82f
Skroderi <bakaminski@wp.pl>
2019-08-03 15:50:36 +0200

    connecting to mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("Api Running"));
 // if deploy on heroku get port if local then 5000
